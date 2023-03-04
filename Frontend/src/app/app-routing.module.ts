4b3eae254c8d8b0ca8e306980b854925504e8496
gisioraelvis <gisioraelvis20@gmail.com>
2023-03-04 23:46:07 +0300

    user profile route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,17 +3,62 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './Services/auth-guard.service';
 
 const routes: Routes = [
-  {path:'', loadComponent:()=>import('./home/home.component').then(c=>c.HomeComponent)},
-  {path:'register', loadComponent:()=>import('./Auth/register/register.component').then(c=>c.RegisterComponent)},
-  {path:'login',  loadComponent:()=>import('./Auth/login/login.component').then(c=>c.LoginComponent)},
-  {path:'book',  loadComponent:()=>import('./Bookings/booking/booking.component').then(c=>c.BookingComponent)},
-  {path:'book/:id', canActivate:[AuthGuardService], loadComponent:()=>import('./Bookings/single-booking/single-booking.component').then(c=>c.SingleBookingComponent)},
-  {path:'book/:id/edit', canActivate:[AuthGuardService], loadComponent:()=>import('./Bookings/update-booking/update-booking.component').then(c=>c.UpdateBookingComponent)},
-  {path:'**', loadComponent:()=>import('./page-not-found/page-not-found.component').then(c=>c.PageNotFoundComponent)}
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./Auth/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./Auth/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./profile/profile.component').then((c) => c.ProfileComponent),
+  },
+  {
+    path: 'book',
+    loadComponent: () =>
+      import('./Bookings/booking/booking.component').then(
+        (c) => c.BookingComponent
+      ),
+  },
+  {
+    path: 'book/:id',
+    canActivate: [AuthGuardService],
+    loadComponent: () =>
+      import('./Bookings/single-booking/single-booking.component').then(
+        (c) => c.SingleBookingComponent
+      ),
+  },
+  {
+    path: 'book/:id/edit',
+    canActivate: [AuthGuardService],
+    loadComponent: () =>
+      import('./Bookings/update-booking/update-booking.component').then(
+        (c) => c.UpdateBookingComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./page-not-found/page-not-found.component').then(
+        (c) => c.PageNotFoundComponent
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
